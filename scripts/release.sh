d034b27e7b8fdcc2483fd2eace609303ce4eb8e1
Awe Morris <awe@noctvm.io>
2025-07-18 22:35:45 +0900

    Updated the release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -46,44 +46,44 @@ cd ..
 # Linux
 #
 
-docker build -t noctscript-build ./scripts
+# docker build -t noctscript-build ./scripts
 
-docker run --rm -it --privileged -v "$PWD":/src -w /src noctscript-build bash -c '
-    flatpak-builder --arch=aarch64 --repo=myrepo --force-clean build-dir ./resources/com.noctscript.NoctScript.yml
-    flatpak build-bundle myrepo NoctScript.flatpak com.noctscript.NoctScript --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
-'
-mv NoctScript.flatpak dist/NoctScript-Linux-aarch64.flatpak
-rm -rf build-dir
+# docker run --rm -it --privileged -v "$PWD":/src -w /src noctscript-build bash -c '
+#     flatpak-builder --arch=aarch64 --repo=myrepo --force-clean build-dir ./resources/com.noctscript.NoctScript.yml
+#     flatpak build-bundle myrepo NoctScript.flatpak com.noctscript.NoctScript --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+# '
+# mv NoctScript.flatpak dist/NoctScript-Linux-aarch64.flatpak
+# rm -rf build-dir
 
-docker run --rm -it --privileged -v "$PWD":/src -w /src noctscript-build bash -c '
-    flatpak-builder --arch=x86_64 --repo=myrepo --force-clean build-dir ./resources/com.noctscript.NoctScript.yml
-    flatpak build-bundle myrepo NoctScript.flatpak com.noctscript.NoctScript --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
-'
-mv NoctScript.flatpak dist/NoctScript-Linux-x86_64.flatpak
-rm -rf build-dir
+# docker run --rm -it --privileged -v "$PWD":/src -w /src noctscript-build bash -c '
+#     flatpak-builder --arch=x86_64 --repo=myrepo --force-clean build-dir ./resources/com.noctscript.NoctScript.yml
+#     flatpak build-bundle myrepo NoctScript.flatpak com.noctscript.NoctScript --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+# '
+# mv NoctScript.flatpak dist/NoctScript-Linux-x86_64.flatpak
+# rm -rf build-dir
 
 #
 # NoctVM
 #
 
-cd bin
-zip -r ../dist/Noct2D-stable.zip \
-    hash \
-    noct2d-x86.exe \
-    noct2d-arm64.exe \
-    NoctVM.dmg \
-    noctvm-linux-aarch64 \
-    noctvm-linux-x86_64 \
-    macos-src.tar.gz \
-    ios-src.tar.gz \
-    android-src.tar.gz \
-    wasm.tar.gz \
-    noctcli-x86.exe \
-    noctcli-arm64.exe \
-    noctcli-macos \
-    noctcli-linux-aarch64 \
-    noctcli-linux-x86_64
-cd ..
+# cd bin
+# zip -r ../dist/Noct2D-stable.zip \
+#     hash \
+#     noct2d-x86.exe \
+#     noct2d-arm64.exe \
+#     NoctVM.dmg \
+#     noctvm-linux-aarch64 \
+#     noctvm-linux-x86_64 \
+#     macos-src.tar.gz \
+#     ios-src.tar.gz \
+#     android-src.tar.gz \
+#     wasm.tar.gz \
+#     noctcli-x86.exe \
+#     noctcli-arm64.exe \
+#     noctcli-macos \
+#     noctcli-linux-aarch64 \
+#     noctcli-linux-x86_64
+# cd ..
 
 #
 # GitHub Release
@@ -93,8 +93,5 @@ VERSION="$(date +%Y%m%d)-dev+$(git rev-parse --short HEAD)"
 gh release create $VERSION \
     dist/NoctScript-Windows-x64.zip \
     dist/NoctScript-macOS-universal.zip \
-    dist/NoctScript-Linux-x86_64.flatpak \
-    dist/NoctScript-Linux-aarch64.flatpak \
-    dist/NoctVM-stable.zip \
     --title "NoctScript $VERSION" \
     --notes "This is a development release of NoctScript!"
